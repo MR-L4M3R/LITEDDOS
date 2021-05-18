c788fc0852c3cd9769d5e7b8208dc74ea6aabcc1
MR.L4M3R <83567167+MR-L4M3R@users.noreply.github.com>
2021-05-18 15:28:16 +0700

    Update DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -21,38 +21,6 @@ if sandi == 'indoxploit' and user == 'Borot':
 else:
     print 'Login GAGAL×, Silahkan hubungi ADMIN'
     wa()
-    restart()
-import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib
-from multiprocessing.pool import ThreadPool
-try:
-    import mechanize
-except ImportError:
-    os.system('pip2 install mechanize')
-else:
-    try:
-        import requests
-    except ImportError:
-        os.system('pip2 install requests')
-
-from requests.exceptions import ConnectionError
-from mechanize import Browser
-reload(sys)
-sys.setdefaultencoding('utf8')
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
-
-def keluar():
-    print '\x1b[1;91m[!] Keluar'
-    os.sys.exit()
-
-
-def jalan(z):
-    for e in z + '\n':
-        sys.stdout.write(e)
-        sys.stdout.flush()
-        time.sleep(0.1)
 
 
 import time
